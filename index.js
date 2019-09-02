bc21cc038313f4694ff7304aaa426b3d5a110b26
Nick <nick.moi@gmail.com>
2019-09-02 13:22:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhipser(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  
+}
